5739022c337c6e165b2fbb0ffd5688be9992d69b
IT24104150 <IT24104150@my.sliit.lk>
2024-11-25 10:14:18 +0530

    Add files via upload

---
+++ Q1A/IT24104150Lab3Q1A.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class IT24104150Lab3Q1A{
+	public static void main (string[] args){
+		// Declare the variples
+		 double priceperitg, Quantity, totalamount;
+	// Create a Scammer object to read input 
+	Scanner input = new Scanner(System.in);
+	// Enter the price per kilogram of rice 
+	System.out.print("Enter the price of 1kg of rices"); 
+	pricePerKg = input.nextDouble();
+	// Enter the number of kilograms they want to buy 
+	System.out.print("Enter the number of kilograms you want to buy: ") ;
+	quantity = input.nextDouble();
+	// Calculate the total amount to be paid 
+	totalAmount = pricepertg * quantity:
+	// Displey the total amount 
+		System.out.println(); 
+	System.out.println("The total amount is: " + totalamuurt);
+	}
+}
